6ace5c8b5e38f953eb4c3ddb53732840e6acbd45
monzer <danesh.monzer@gmail.com>
2024-10-29 22:06:56 +0330

    logs added again to tasks API

--- src/app/api/users/tasks/membership/route.ts
+++ src/app/api/users/tasks/membership/route.ts
@@ -16,6 +16,12 @@ export async function POST(request: NextRequest) {
 
   try {
     const { userId, chatId, taskId, points } = await request.json();
+    console.log("Received request payload:", {
+      userId,
+      chatId,
+      taskId,
+      points,
+    });
 
     if (!userId || !chatId || !taskId || !points) {
       console.warn("Missing parameters:", { userId, chatId, taskId, points });
@@ -38,15 +44,21 @@ export async function POST(request: NextRequest) {
     });
 
     const data: TelegramResponse = await response.json();
+    console.log("Telegram API response:", data);
 
     // Check if the API response is OK and if it contains result data
     if (data.ok && data.result) {
       const isMember = ["creator", "administrator", "member"].includes(
         data.result.status
       );
+      console.log("Membership check:", {
+        isMember,
+        status: data.result.status,
+      });
 
       if (isMember) {
         const { db } = await connectToDatabase();
+        console.log("Connected to database. Updating user data.");
 
         // Update the user with the new task ID in the database
         const updateResult = await db
@@ -55,6 +67,7 @@ export async function POST(request: NextRequest) {
             { telegramId: userId },
             { $addToSet: { tasks: taskId }, $inc: { score: points } }
           );
+        console.log("Database update result:", updateResult);
 
         if (updateResult.matchedCount === 0) {
           console.warn("User not found, task not added");
@@ -64,6 +77,7 @@ export async function POST(request: NextRequest) {
           );
         }
 
+        console.log("User data updated successfully.");
         return NextResponse.json({ success: true, isMember });
       } else {
         console.warn(
